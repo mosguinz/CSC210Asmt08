5d55231aab3ad6e923c4cfe8155607c4fae69580
mosguinz <mos.guinz@gmail.com>
2020-11-08 02:52:52 -0800

    Add test cases for rest of part 2

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -1,6 +1,6 @@
 /*
  * Assignment 08
- * Description: [Part 1 of 3] Rectangle class
+ * Description: [Part 1 of 3] Rectangle test class
  * Name: Kullathon "Mos" Sitthisarnwattanachai
  * Teammate: Amber Hartigan
  * ID: 921425216
@@ -9,5 +9,27 @@
  */
 
 public class RectangleTest {
+    /**
+     * Test cases for the rest of part two.
+     * <p>
+     * The {@link Rectangle} class can be found
+     * <a href="https://github.com/mosguinz-csc210-03/CSC210Asmt08/blob/main/src/Rectangle.java">here</a>.
+     *
+     * @see {@link Rectangle}
+     */
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(4, 40);
+        Rectangle r2 = new Rectangle(3.5, 35.9);
 
+        Rectangle[] rectangles = {r1, r2};
+
+        for (int i = 0; i < rectangles.length; i++) {
+            Rectangle r = rectangles[i];
+            System.out.printf("[Rectangle %d]%n", i + 1);
+            System.out.printf("Width: %.2f%n", r.getWidth());
+            System.out.printf("Height: %.2f%n", r.getHeight());
+            System.out.printf("Area: %.2f%n", r.getArea());
+            System.out.printf("Perimeter: %.2f%n%n", r.getPerimeter());
+        }
+    }
 }
