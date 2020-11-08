28edb5c4f52ff9c370f83952b88bef7413e77f23
mosguinz <mos.guinz@gmail.com>
2020-11-08 03:27:01 -0800

    Add method to prompt employee amount

--- src/CoffeeShopEmployee.java
+++ src/CoffeeShopEmployee.java
@@ -8,8 +8,33 @@
  * Semester: Fall 2020
  */
 
-public class CoffeeShopEmployee {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CoffeeShopEmployee {
+
+    public static final int MIN_EMPLOYEE = 3;
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        promptEmployeeAmount();
+    }
+
+    /**
+     * Prompt and validate the amount of {@link Employee} objects to
+     * instantiate. The minimum (inclusive) amount is set by {@link
+     * #MIN_EMPLOYEE} (3 by default). Satisfies (3)(1).
+     *
+     * @return The valid number of employee to create.
+     */
+    private static int promptEmployeeAmount() {
+        while (true) {
+            System.out.print("Enter the number of employees you want to create: ");
+            int amt = scan.nextInt();
+            if (amt < MIN_EMPLOYEE) {
+                System.out.printf("Invalid amount. Must be at least %d.%n", MIN_EMPLOYEE);
+                continue;
+            }
+            return amt;
+        }
     }
 }
